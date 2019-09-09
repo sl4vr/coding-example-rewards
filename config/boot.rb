d3aef35f12c0e7f150f261ad5408f27a5e4b7dd5
Viacheslav Mefodin <mefodin.v@gmail.com>
2019-09-09 18:15:31 +0300

    Require lib directory

--- config/boot.rb
+++ config/boot.rb
@@ -5,3 +5,6 @@ require 'sinatra'
 
 ENV['ROOT_PATH'] = File.expand_path('../', File.dirname(__FILE__))
 require File.expand_path('application.rb', ENV['ROOT_PATH'])
+
+lib_files = File.join(ENV['ROOT_PATH'] , %w[lib ** *.rb])
+Dir.glob(lib_files).each { |f| require f }
