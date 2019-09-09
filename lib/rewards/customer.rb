178912f6e8b26d84b6294424df8f6812dbc09032
Viacheslav Mefodin <mefodin.v@gmail.com>
2019-09-09 18:41:49 +0300

    Implement Customer model

---
+++ lib/rewards/customer.rb
@@ -0,0 +1,28 @@
+# frozen_string_literal: true
+
+module Rewards
+  # Customer model
+  class Customer
+    CustomerError = Class.new(StandardError)
+    NoNameError = Class.new(CustomerError)
+    NonNumericScoreError = Class.new(CustomerError)
+
+    attr_reader :name, :score
+
+    def initialize(name)
+      raise NoNameError, 'No name given' if name.blank?
+
+      @name = name
+      @score = 0.0
+    end
+
+    def add_score(score)
+      unless score.is_a?(Numeric)
+        raise NonNumericScoreError,
+              "Score must be Numeric, but instead is #{score.class}" 
+      end
+
+      @score += score
+    end
+  end
+end
---
+++ spec/lib/rewards/customer_spec.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'spec_helper'
+
+describe Rewards::Customer do
+  let(:name) { 'Test' }
+  subject(:customer) { Rewards::Customer.new(name) }
+
+  describe '.new' do
+    it { is_expected.to be_kind_of(Rewards::Customer) }
+
+    it 'has name' do
+      expect(customer.name).to eq(name)
+    end
+
+    it 'has 0.0 score' do
+      expect(customer.score).to eq(0.0)
+    end
+
+    context 'when no name given' do
+      let(:name) { nil }
+
+      it 'raises NoNameError' do
+        expect {subject}.to raise_error(Rewards::Customer::NoNameError)
+      end
+    end
+  end
+
+  describe '#add_score' do
+    it 'adds score' do
+      score_was = customer.score
+      score = 3.2
+
+      customer.add_score(score)
+
+      expect(customer.score).to eq(score_was + score)
+    end
+
+    context 'when non-numeric given' do
+      it 'raises NonNumericScoreError' do
+        expect {
+          customer.add_score('10')
+        }.to raise_error(Rewards::Customer::NonNumericScoreError)
+      end
+    end
+  end
+end
