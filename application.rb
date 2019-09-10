2b054be15f1132185b5cb1cc55d7a5537781dce3
Viacheslav Mefodin <mefodin.v@gmail.com>
2019-09-10 18:19:53 +0300

    Add web service for rewards calculation

--- application.rb
+++ application.rb
@@ -5,4 +5,24 @@ class Application < Sinatra::Base
   get '/?' do
     erb :index
   end
+
+  post '/upload' do
+    file = params[:file][:tempfile]
+    log = File.read(file)
+
+    customer_repository = Rewards::CustomerRepository.new
+    recommendation_repository = Rewards::RecommendationRepository.new
+    log_parser = Rewards::Parser
+
+    calculator = Rewards::Calculator.new(
+      input: log,
+      parser: log_parser,
+      customers: customer_repository,
+      recommendations: recommendation_repository
+    )
+
+    @scores = calculator.calculate
+
+    @scores.to_json
+  end
 end
